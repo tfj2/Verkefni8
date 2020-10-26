58debc27c3fe24dd40a84a0fc0bb6daa898c5131
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2020-10-26 12:13:27 +0000

    Verkefni 8 sýnilausn

---
+++ scripts.js
@@ -0,0 +1,55 @@
+/**
+ * Verkefni 8 – Caesar dulmál með vefviðmóti
+ *
+ * Verður að passa _nákvæmlega_ við gefið HTML, mun annars brotna.
+ * Þ.e.a.s., ekki þarf að skrifa meðhöndlun á HTML elementum sem vantar
+ */
+
+/**
+ * Kóðar streng með því að hliðra honum um n stök.
+ *
+ * @param {string} str Strengur sem skal kóða, aðeins stafir í stafrófi
+ * @param {number} n Hliðrun, heiltala á bilinu [0, lengd stafrófs]
+ * @param {string} alphabet Stafróf sem afkóða á út frá
+ * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
+ */
+function encode(str, n, alphabet = '') {
+  return '';
+}
+
+/**
+ * Afkóðar streng með því að hliðra honum um n stök.
+ *
+ * @param {string} str Strengur sem skal afkóða, aðeins stafir í stafrófi
+ * @param {number} n Hliðrun, heiltala á bilinu [0, lengd stafrófs]
+ * @param {string} alphabet Stafróf sem afkóða á út frá
+ * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
+ */
+function decode(str, n, alphabet = '') {
+  return '';
+}
+
+const Caesar = (() => {
+  // Default stafróf, uppfært þegar slegið inn í "alphabet"
+  let alphabet = 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ';
+
+  // Default type, uppfært af radio input
+  let type = 'encode';
+
+  // Default hliðrun, uppfært af "shift"
+  let shift = 3;
+
+  function init(el) {
+    // Setja event handlera á viðeigandi element
+  }
+
+  return {
+    init,
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ceasarForm = document.querySelector('.ceasar');
+
+  Caesar.init(ceasarForm);
+});
