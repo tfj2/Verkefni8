dc83b5acf9229a580267f09d1d65e8f615aee6d9
unknown <tfj2@hi.is>
2020-11-03 21:24:22 +0000

    forritun

--- scripts.js
+++ scripts.js
@@ -13,9 +13,21 @@
  * @param {string} alphabet Stafróf sem afkóða á út frá
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
-function encode(str, n, alphabet = '') {
-  return '';
+function encode(str, n, alphabet1) {
+  let newStr = '';
+  let i;
+  alphabet1 = alphabet1.toLowerCase()
+  for (i = 0; i < str.length; i++) {
+    if (alphabet1.indexOf(str.charAt(i)) + n > alphabet1.length - 1) {
+      newStr += alphabet1.charAt(n - alphabet1.length + alphabet1.indexOf(str.charAt(i)));
+    } else {
+      newStr += alphabet1.charAt(alphabet1.indexOf(str.charAt(i)) + n);
+    }
+  }
+  newStr.toUpperCase();
+  return newStr;
 }
+
 
 /**
  * Afkóðar streng með því að hliðra honum um n stök.
@@ -25,19 +37,61 @@ function encode(str, n, alphabet = '') {
  * @param {string} alphabet Stafróf sem afkóða á út frá
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
-function decode(str, n, alphabet = '') {
-  return '';
+function decode(str, n, alphabet1) {
+  let m = alphabet1.length - n;
+  return encode(str, m);
 }
 
 const Caesar = (() => {
   // Default stafróf, uppfært þegar slegið inn í "alphabet"
-  let alphabet = 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ';
+  let theAlphabet = 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ';
+  alphabet.addEventListener('input', () => {
+    theAlphabet = alphabet.value;
+    console.log(theAlphabet)
+    if(type === 'encode')
+    console.log(encode(Gildi, shift, theAlphabet))
+   else
+    console.log(decode(Gildi, shift, theAlphabet))
+  });
 
   // Default type, uppfært af radio input
   let type = 'encode';
+  const radios = document.querySelectorAll('input[type=radio]');
+  function radioChanged(e) {
+    type = `${e.target.value}`;
+    console.log(type)
+  }
+
+  for (let i = 0; i < radios.length; i++){
+    radios[i].addEventListener('change', radioChanged);
+  }
 
   // Default hliðrun, uppfært af "shift"
   let shift = 3;
+  const shifts = document.querySelectorAll('input[type=range]');
+  function shiftChanged(e) {
+    shift = `${e.target.value}`;
+    console.log(shift)
+    if(type === 'encode')
+    console.log(encode(Gildi, shift, theAlphabet))
+   else
+    console.log(decode(Gildi, shift, theAlphabet))
+  }
+
+  for (let i = 0; i < shifts.length; i++) {
+    shifts[i].addEventListener('change', shiftChanged);
+  }
+
+  let Gildi = '';
+  input.addEventListener('input', () => {
+    Gildi = input.value;
+    console.log(Gildi)
+    if(type === 'encode')
+     console.log(encode(Gildi, shift, theAlphabet))
+    else
+     console.log(decode(Gildi, shift, theAlphabet))
+  });
+
 
   function init(el) {
     // Setja event handlera á viðeigandi element
